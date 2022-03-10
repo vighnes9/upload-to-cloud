a811ff6e92e000ede5cb0688f025f1af2bd4045d
vighnes9 <96604633+vighnes9@users.noreply.github.com>
2022-03-10 17:51:20 +0530

    Add files via upload

---
+++ c101/main.py
@@ -0,0 +1,24 @@
+import dropbox
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+
+def main():
+    access_token = 'N-l_nRoXgCYAAAAAAAAAAbM4OQVEbHmS5RhDktFM32JSx7vWghfs9fjKpR-ClPrF'
+    transferData = TransferData(access_token)
+
+    file_from = 'test.txt'
+    file_to = '/test_dropbox/test.txt'  # The full path to upload the file to, including the file name
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
+
+if __name__ == '__main__':
+    main()
